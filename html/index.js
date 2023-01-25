94b5ce1d8a8f337a3dfeb0d0a8540676cb2b65a5
Joseph Ditton <jditton.atomic@gmail.com>
2023-01-25 10:20:54 -0700

    adds mouse events

--- html/index.js
+++ html/index.js
@@ -1,6 +1,41 @@
 const fuddifyButton = document.getElementById("fuddify-button");
-fuddifyButton.addEventListener("click", () => {
+
+function fuddify(text) {
+  return text.replaceAll("r", "w").replaceAll("R", "W");
+}
+
+const input = document.getElementById("user-input");
+
+const callback = (e) => {
+  e.stopPropagation();
   const div = document.getElementById("output");
-  const input = document.getElementById("user-input");
-  div.innerHTML = input.value;
+  const fuddifiedText = fuddify(input.value);
+  input.value = "";
+  div.innerHTML = fuddifiedText;
+}
+
+
+input.addEventListener("input", (e) => {
+  console.log(e);
 });
+
+input.addEventListener("blur", (e) => {
+  console.log(e);
+});
+
+input.addEventListener("focus", (e) => {
+  console.log(e);
+});
+
+fuddifyButton.addEventListener("click", callback);
+fuddifyButton.addEventListener("click", console.log);
+
+// fuddifyButton.removeEventListener("click", callback);
+
+const divs = document.getElementsByTagName("div");
+console.log(divs);
+
+const bigFontElements = document.getElementsByClassName("big-font");
+console.log(bigFontElements);
+
+
---
+++ mouseevents/index.js
@@ -0,0 +1,8 @@
+document.body.addEventListener("click", (e) => {
+  console.log(e);
+});
+
+
+document.body.addEventListener("mousedown", console.log);
+document.body.addEventListener("mousemove", console.log);
+document.body.addEventListener("mouseup", console.log);
